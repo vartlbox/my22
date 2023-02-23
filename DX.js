1813440bb0e94b7341181adb57668e0ecb269c90
vartlbox <125939870+vartlbox@users.noreply.github.com>
2023-02-23 17:57:43 +0300

    Add files via upload

---
+++ DX.js
@@ -0,0 +1,113 @@
+document.getElementById("myButton").onclick = function(){
+// ama dogmakaaxataeysh
+
+
+
+
+//daatabays
+
+
+var number = "number"
+var zhmara = "ژمارە"
+
+
+var face = "face"
+var roxsar = "رۆخسار"
+
+var book = "book"
+var kteyb = "کتێب"
+
+var pen = "pen"
+var penws = "پێنووس"
+
+
+var mother = "mother"
+var daek = "دایک"
+
+var father = "father"
+var bawk = "باوک"
+
+
+
+
+
+
+
+
+//daatabays
+
+
+  
+// ama enptakamana
+  var u = document.getElementById("myText").value ; 
+// ama enptakamana
+
+ 
+
+if(u == face){
+
+  document.write(roxsar)
+  
+  }
+  
+   
+  
+  
+  
+  
+  
+  if(u == number){
+
+    document.write(zhmara)
+    
+    }
+
+
+
+    if(u == book){
+
+      document.write(kteyb)
+      
+      }
+
+      if(u == pen){
+
+        document.write(penws)
+        
+        }
+
+
+
+
+        if(u == mother){
+
+          document.write(daek)
+          
+          }
+
+
+          if(u == father){
+
+            document.write(bawk)
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
